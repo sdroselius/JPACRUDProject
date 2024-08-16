2e02deea98d1b440180768d032d0881c73444355
Rob Roselius <roselius@skilldistillery.com>
2024-08-16 16:44:18 -0600

    Create Boot MVC project, connect JPA project dependency

---
+++ Fun4Dogs/src/main/java/com/skilldistillery/dogparks/Fun4DogsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.dogparks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fun4DogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fun4DogsApplication.class, args);
+	}
+
+}
