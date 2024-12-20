036cf5d1f325b40227e28a6bc8b7eb9370f0d1e6
Rob Roselius <roselius@skilldistillery.com>
2024-12-20 16:05:43 -0700

    Create Boot MVC project, link JPA project dependency

---
+++ ComedyShows/src/main/java/com/skilldistillery/comedy/ComedyShowsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.comedy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComedyShowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComedyShowsApplication.class, args);
+	}
+
+}
---
+++ ComedyShows/src/test/java/com/skilldistillery/comedy/ComedyShowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.comedy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComedyShowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
