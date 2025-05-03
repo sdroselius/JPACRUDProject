f141d9c635325aa680ab099908ad7fb67109278e
Rob Roselius <roselius@skilldistillery.com>
2025-05-02 18:17:51 -0600

    Create Spring Boot MVC project, configure dependencies

---
+++ GoodBugs/src/main/java/com/skilldistillery/goodbugs/GoodBugsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.goodbugs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodBugsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodBugsApplication.class, args);
+	}
+
+}
